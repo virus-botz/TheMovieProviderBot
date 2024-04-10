155bcb7a3adc8f1b645219073aa898131cdc93e3
virus-botz <158062548+virus-botz@users.noreply.github.com>
2024-04-10 09:26:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -292,7 +292,7 @@ async def start(client, message):
     elif data.split("-", 1)[0] == "verify":
         userid = data.split("-", 2)[1]
         token = data.split("-", 3)[2]
-        if str(message.from_user.id) != str(userid):
+        if user not in PREMIUM_USER :        
             return await message.reply_text(
                 text="<b>Invalid link or Expired link !</b>",
                 protect_content=True
@@ -451,7 +451,7 @@ async def start(client, message):
         else:
             chat_id = temp.SHORT.get(user_id)
         settings = await get_settings(chat_id)
-        if not await db.has_premium_access(user_id) #Don't change anything without my permission @CoderluffyTG
+        if not await db.has_premium_access(user_id) and settings['is_shortlink'] : #Don't change anything without my permission @CoderluffyTG
             files_ = await get_file_details(file_id)
             files = files_[0]
             g = await get_shortlink(chat_id, f"https://telegram.me/{temp.U_NAME}?start=file_{file_id}")
