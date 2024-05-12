e04db56f479c77314aa25f8fe96062d5ef907c6d
virus-botz <158062548+virus-botz@users.noreply.github.com>
2024-05-12 21:21:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,8 +59,8 @@ VERIFY = bool(environ.get('VERIFY', True)) # Verification On ( True ) / Off ( Fa
 HOWTOVERIFY = environ.get('HOWTOVERIFY', 'https://t.me/kaisekhole/8') # How to open tutorial link for verification
 
 # Others
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'Zxlink.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '72fe2cd23d0e62ef0ab8efa493a78dff1f311cc2')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'Krishnalink.com ')
+SHORTLINK_API = environ.get('SHORTLINK_API', '6ac6f2011d1ce803a0f9073721e1dff72e47c0d3')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "10")
